f56f04470fe4abc4cec0bbaea7f5d981a5001816
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2023-10-11 21:55:53 +0600

    Add project files.

---
+++ Module6H/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module6H
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
