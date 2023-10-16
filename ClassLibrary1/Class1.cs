63c83fef415d99b3d00434f729181e9c8faa9a0a
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2023-10-16 18:39:23 +0600

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyClassLib.WordOfTanks
+{
+    public class Tank
+    {
+        private string tankName;
+        private int ammoLevel;
+        private int armorLevel;
+        private int maneuverabilityLevel;
+        private Random random;
+
+        public Tank(string name)
+        {
+            random = new Random();
+            tankName = name;
+            ammoLevel = random.Next(101);
+            armorLevel = random.Next(101);
+            maneuverabilityLevel = random.Next(101);
+        }
+
+        // Implement operator overloads (^) for tank battles.
+        public static bool operator >(Tank tank1, Tank tank2)
+        {
+            int parametersBetterInTank1 = 0;
+
+            if (tank1.ammoLevel > tank2.ammoLevel)
+                parametersBetterInTank1++;
+            if (tank1.armorLevel > tank2.armorLevel)
+                parametersBetterInTank1++;
+            if (tank1.maneuverabilityLevel > tank2.maneuverabilityLevel)
+                parametersBetterInTank1++;
+
+            return parametersBetterInTank1 >= 2;
+        }
+
+        public static bool operator <(Tank tank1, Tank tank2)
+        {
+            return tank2 > tank1;
+        }
+
+        public string GetTankParameters()
+        {
+            return $"Tank: {tankName}, Ammo: {ammoLevel}%, Armor: {armorLevel}%, Maneuverability: {maneuverabilityLevel}%";
+        }
+    }
+}
---
+++ homework7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MyClassLib.WordOfTanks;
+
+class Program
+{
+    static void Main()
+    {
+        Tank t34 = new Tank("T-34");
+        Tank pantera = new Tank("Pantera");
+
+        Console.WriteLine("Tank Battle Simulation:");
+        Console.WriteLine(t34.GetTankParameters());
+        Console.WriteLine(pantera.GetTankParameters());
+
+        if (t34 > pantera)
+        {
+            Console.WriteLine($"{t34.GetTankParameters()} wins the battle against {pantera.GetTankParameters()}");
+        }
+        else
+        {
+            Console.WriteLine($"{pantera.GetTankParameters()} wins the battle against {t34.GetTankParameters()}");
+        }
+
+        Console.ReadKey();
+    }
+}
