b51f01b35183bad80270b674ba19140a7bf6de0f
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2023-10-23 09:34:52 +0600

    NEw commit

--- example1/Program.cs
+++ example1/Program.cs
@@ -10,7 +10,7 @@ namespace example1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Seka");
         }
     }
 }
